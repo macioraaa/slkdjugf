b9f56a26c082913167344f96e52faba0ba9cf888
macioraaa <156977504+macioraaa@users.noreply.github.com>
2025-01-30 16:30:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["☠ Jebac Ruch Chorzów", "🚩 SPARTA KATOWICE TUDUDUDUDUDU"];
+const statusMessages = ["Słucha Jebać Cweli"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
